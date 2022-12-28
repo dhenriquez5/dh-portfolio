ade0923f50963b5434dbd5edf7c022d076533a9a
david.henriquez <dhenriquezturcios@gmail.com>
2022-12-27 19:56:12 -0500

    changin routing structure

--- pages/blog-david-henriquez/index.tsx
+++ pages/blog-david-henriquez/index.tsx
@@ -1,9 +1,9 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
-import { Layout } from "../components/Layout";
-import { posts } from "../utils/data";
-import { PostCard } from "../components/PostCard";
+import { Layout } from "../../components/Layout";
+import { posts } from "../../utils/data";
+import { PostCard } from "../../components/PostCard";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,8 +11,8 @@ export default function Blog() {
   return (
     <>
       <Layout
-        description="Blog David Henriquez"
         title="Blog David Henriquez"
+        description="Blogs related to different technologies for web development, how to start, best practices and more..."
         footer={false}
       >
         <h3 className="text-center text-light">My Blog</h3>
